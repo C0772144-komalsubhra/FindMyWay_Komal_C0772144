2242513752395280fa1b540e83d436350b8a29ee
Komal Subhra <60164715+C0772144-komalsubhra@users.noreply.github.com>
2020-06-13 18:22:54 -0400

    added switch in func route

--- FindMyWay_Komal_C0772144/ViewController.swift
+++ FindMyWay_Komal_C0772144/ViewController.swift
@@ -83,3 +83,46 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
             self.mapView.addAnnotation(annotation)
         }
+     func route()
+    {
+            self.mapView.removeOverlays(self.mapView.overlays)
+           
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!), addressDictionary: nil))
+        
+        if(aLat == nil || aLon == nil)
+        {
+                let alertController = UIAlertController(title: "Error", message:
+                "No destination selected", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+                self.present(alertController, animated: true, completion: nil)
+        }
+        else
+        {
+              
+                request.destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: aLat as! CLLocationDegrees, longitude: aLon as! CLLocationDegrees), addressDictionary: nil))
+                request.requestsAlternateRoutes = false
+        }
+       
+        switch segmentType.selectedSegmentIndex
+        {
+            case 0:
+                request.transportType = .walking
+            case 1:
+                request.transportType = .automobile
+            default:
+                break
+        }
+        let directions = MKDirections(request: request)
+
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else { return }
+
+            for route in unwrappedResponse.routes {
+                self.mapView.addOverlay(route.polyline)
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+            }
+        }
+    }
+}
